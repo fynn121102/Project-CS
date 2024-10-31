06f0288e58e0390c1a960b6c6dc490a5497283ed
fynn121102 <fynn.hirrlinger@student.unisg.ch>
2024-10-31 14:26:15 +0100

    pushhhhh

--- app.py
+++ app.py
@@ -2,3 +2,4 @@ import streamlit as st
 st.write ("hooray, weve connected everything")
 st.write ("now I can send text to the application! Isnt that fun")
 st.write ("does it also work without pushing?")
+st.write ("no, you have to push it!")
